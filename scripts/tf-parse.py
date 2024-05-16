270516fd94e6b6bdb24172414c62d865d0a540cc
Lukáš Zachar <lzachar@redhat.com>
2024-05-16 18:44:35 +0200

    Better reporting of URL
    Make task report ARTIFACTS_URL, TEST_OUTPUT and REQUEST_URL.

---
+++ scripts/tf-parse.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import datetime
+import json
+import sys
+
+def write_js(obj, f_path):
+    with open(f_path, 'w') as f:
+        json.dump(obj, f)
+def write(obj, f_path):
+    with open(f_path, 'w') as f:
+        f.write(str(obj))
+
+with open(sys.argv[1]) as f:
+    request = json.load(f)
+
+f_artifacts_url = sys.argv[2]
+f_test_output = sys.argv[3]
+
+# Report the URL to artifacts
+artifacts = request['run']['artifacts']
+if artifacts:
+    write(artifacts, f_artifacts_url) 
+
+
+# Process the results..
+test_output = {
+    'result': 'ERROR',
+    "timestamp": str(int(datetime.datetime.fromisoformat(request['updated']).timestamp())),
+    "successes": 0, 
+    "failures": 0,
+    "warnings": 0
+}
+    
+# timestm
+
+state = request['state']    
+if state != 'complete':
+    test_output['note'] = f'TF request finished as {state}'
+try:
+    overall = request['result']['overall']
+except (TypeError, KeyError):
+    overall = 'unknown'
+
+# Nothing will create 'WARNING'
+overall2tekton = {
+'passed': 'SUCCESS',
+'failed': 'FAILURE',
+'skipped': 'SKIPPED',
+'error': 'ERROR',
+'unknown': 'ERROR',
+}
+
+test_output['result'] = overall2tekton[overall]
+write_js(test_output, f_test_output)
+
+print(test_output)
+
+if overall != 'passed':
+    raise SystemExit(1)
--- scripts/tf-watch.py
+++ scripts/tf-watch.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python3
 import requests
 import sys
-import time          
+import time  
+import json
+
+SLEEP = 60
 
 while True:
     ret = requests.get(sys.argv[1]).json()
     state = ret['state']
     print(f'{state=}', file=sys.stderr)
     if state not in ['new', 'queued', 'running']:
-        print(f'Stop waiting, becasue {state=}', file=sys.stderr)
+        print(f'\nStop waiting as {state=}', file=sys.stderr)
         break
-    print(time.gmtime(), file=sys.stderr)
-    time.sleep(60) 
+    time.sleep(SLEEP) 
 
-# not running...
-if state != 'complete':
-    print('Not completed job!', file=sys.stderr)
-    raise SystemExit(2)
-    
-overal_result = ret['result']['overall']
-print(f'{overal_result=}', file=sys.stderr)
-
-if overal_result != 'passed':
-    print('Not passed', file=sys.stderr)
-    raise SystemExit(1)
-
+f_out = sys.argv[2]
+with open(f_out, 'w') as f:
+    json.dump(ret, f)
+print(f'Wrote requests json to "{f_out}"', file=sys.stderr)    
